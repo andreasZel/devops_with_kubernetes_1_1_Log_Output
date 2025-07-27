4cd9a7c2d10ad97877af8e78279a4b8e2922d9bf
andreas <antreaszel@gmail.com>
2025-07-28 00:13:06 +0300

    created new endpoint to return pongs and removed saving in file

--- PongOutput/pong.js
+++ PongOutput/pong.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 const http = require("http");
-const fs = require("fs");
-const path = require('path');
+// const fs = require("fs");
+// const path = require('path');
 var counter = 0;
 var logString = '';
 
-const logPath = path.join(__dirname, 'logs', 'counter.txt');   
+// const logPath = path.join(__dirname, 'logs', 'counter.txt');   
 
 const server = http.createServer((req, res) => {
 
@@ -13,17 +13,31 @@ const server = http.createServer((req, res) => {
         try {
             logString = `pong ${counter}`;
             counter++;
-            
-            fs.writeFile(logPath, logString, 'utf8', (err) => {
-                if (err) {
-                    console.error('Error writing file:', err);
-                    return;
-                }
-                console.log(`File written successfully in ${logPath}!`);
-            });
 
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            // fs.writeFile(logPath, logString, 'utf8', (err) => {
+            //     if (err) {
+            //         console.error('Error writing file:', err);
+            //         return;
+            //     }
+            //     console.log(`File written successfully in ${logPath}!`);
+            // });
+
+            // res.writeHead(200, { 'Content-Type': 'text/plain' });
             res.write(`pong ${counter}`);
+            res.end();
+        } catch (e) {
+            console.log(e);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.write(`Error saving counter`);
+            res.end("Error reading file");
+        }
+
+    } else if (req?.method === 'GET' && req.url === '/pings') {
+
+        try {
+            logString = `Ping / Pongs: ${counter}`;
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.write(logString);
             res.end();
         } catch (e) {
             console.log(e);
