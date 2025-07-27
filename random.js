55bb211cee1d4ec8c340baed00e8fb876e080878
andreas <antreaszel@gmail.com>
2025-07-28 00:13:58 +0300

    added server and endpoint to return number of pings and timestamp with string by fetching the pings from the new /pings endpoint

--- random.js
+++ random.js
@@ -1,25 +1,55 @@
 #!/usr/bin/env node
 const crypto = require("crypto");
-const fs = require("fs");
-const path = require('path');
+const http = require("http");
+// const fs = require("fs");
+// const path = require('path');
 const id = crypto.randomBytes(20).toString('hex');
 var logString = '';
 
-const logPath = path.join(__dirname, 'logs', 'text.logs');        
+// const logPath = path.join(__dirname, 'logs', 'text.logs');        
 
 setInterval(() => {
     try {
         logString = `${new Date().toISOString()}: ${id}`;
 
-        fs.writeFile(logPath, logString, 'utf8', (err) => {
-            if (err) {
-                console.error('Error writing file:', err);
-                return;
-            }
-            console.log(`File written successfully in ${logPath}!`);
-        });
-    } catch(e) {
+        // fs.writeFile(logPath, logString, 'utf8', (err) => {
+        //     if (err) {
+        //         console.error('Error writing file:', err);
+        //         return;
+        //     }
+        //     console.log(`File written successfully in ${logPath}!`);
+        // });
+    } catch (e) {
         console.log(e);
     }
 }, 5e3);
+
+const server = http.createServer(async (req, res) => {
+
+    if (req?.method === 'GET' && req.url === '/') {
+        try {
+            const pings = await fetch('http://ping-pong-svc:2020/pings');
+            const pingsText = await pings.text();
+            res.write(`${logString}.\n${pingsText}`);
+           
+            res.end();
+        } catch (e) {
+            console.log(e);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.write(`Error returning response`);
+            res.end("Error reading file");
+        }
+
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+
+});
+
+server.listen(3004, () => {
+    console.log('Started Server on Port 3004');
+})
+
+
 
