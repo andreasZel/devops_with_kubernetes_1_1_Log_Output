f93a7eaa152f1b7e9fab26439b4aad505fcf500f
andreas <antreaszel@gmail.com>
2025-08-10 00:21:22 +0300

    updated pong javascript file to use the env vars connect to db, and save counter there

--- PongOutput/pong.js
+++ PongOutput/pong.js
@@ -2,17 +2,63 @@
 const http = require("http");
 // const fs = require("fs");
 // const path = require('path');
-var counter = 0;
+// var counter = 0;
 var logString = '';
+require('dotenv').config();
+const { Pool } = require('pg');
 
 // const logPath = path.join(__dirname, 'logs', 'counter.txt');   
 
-const server = http.createServer((req, res) => {
+async function dbInitAndConnect() {
+    var client = null;
+
+    console.log('Connecting to db');
+
+    try {
+        client = new Pool({
+            host: process.env.POSTGRES_HOST,
+            port: process.env.POSTGRES_PORT,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+        })
+
+
+    } catch (e) {
+        console.log('Error Connecting to db: ', e)
+        return null;
+    }
+
+    try {
+        await client.query(` CREATE TABLE IF NOT EXISTS pingcounter (
+        id BIGSERIAL PRIMARY KEY,
+        counter INT
+      )`);
+
+        console.log("✅ Table pingcounter check/creation complete.");
+    } catch (err) {
+        console.error("❌ Error creating pingcounter:", err);
+    }
+
+    return client;
+}
+
+const dbpool = await dbInitAndConnect();
+
+const server = http.createServer(async (req, res) => {
 
     if (req?.method === 'GET' && req.url === '/pingpong') {
         try {
-            logString = `pong ${counter}`;
-            counter++;
+            const pingsDbResult = await dbpool.query(`SELECT counter from pingcounter`);
+
+            if (pingsDbResult?.rows?.length === 0) {
+                res.write(`No pong yet`);
+                await dbpool.query(`INSERT INTO pingcounter(counter) VALUES($1)`, [1]);
+            } else {
+                const counter = pingsDbResult.rows[0]?.counter;
+                res.write(`pong ${pingsDbResult.rows[0]?.counter}`);
+                await dbpool.query(`UPDATE pingcounter SET counter = $1`, [(counter + 1)]);
+            }
 
             // fs.writeFile(logPath, logString, 'utf8', (err) => {
             //     if (err) {
@@ -22,28 +68,34 @@ const server = http.createServer((req, res) => {
             //     console.log(`File written successfully in ${logPath}!`);
             // });
 
-            // res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.write(`pong ${counter}`);
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
             res.end();
         } catch (e) {
             console.log(e);
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.write(`Error saving counter`);
-            res.end("Error reading file");
+            res.end("Error reading counter");
         }
 
     } else if (req?.method === 'GET' && req.url === '/pings') {
 
         try {
-            logString = `Ping / Pongs: ${counter}`;
+            const pingsDbResult = await dbpool.query(`SELECT counter from pingcounter`);
+            if (pingsDbResult?.rows?.length === 0) {
+                res.write(`No Ping / Pongs yet`);
+            } else {
+                const counter = pingsDbResult.rows[0]?.counter;
+                logString = `Ping / Pongs: ${counter}`;
+                res.write(logString);
+            }
+            
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.write(logString);
             res.end();
         } catch (e) {
             console.log(e);
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.write(`Error saving counter`);
-            res.end("Error reading file");
+            res.end("Error reading counter");
         }
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
